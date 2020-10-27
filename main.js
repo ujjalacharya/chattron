9a8db1d3a506c7ff3c5b8451d03f5ece2c244089
Ujjal Acharya <ujjal.acharya@amniltech.com>
2020-10-27 13:11:15 +0545

    add browser window

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log('Hello World');
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    backgroundColor: "white",
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(createWindow);
