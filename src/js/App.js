c972227b4c02c3f87b082dfcacf827675fd2708f
Ujjal Acharya <ujjal.acharya@amniltech.com>
2020-10-29 18:18:29 +0545

    init redux store

--- src/js/App.js
+++ src/js/App.js
@@ -2,6 +2,10 @@ import React from "react";
 
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
+import { Provider } from "react-redux";
+
+import configureStore from "./store";
+
 import Navbar from "./components/Navbar";
 import ChatView from "./views/Chat";
 import HomeView from "./views/Home";
@@ -9,29 +13,33 @@ import LoginView from "./views/Login";
 import RegisterView from "./views/Register";
 import SettingsView from "./views/Settings";
 
+const store = configureStore();
+
 export default function App() {
   return (
-    <Router>
-      <Navbar />
-      <div className="content-wrapper">
-        <Switch>
-          <Route path="/settings">
-            <SettingsView />
-          </Route>
-          <Route path="/login">
-            <LoginView />
-          </Route>
-          <Route path="/register">
-            <RegisterView />
-          </Route>
-          <Route path="/chat/:id">
-            <ChatView />
-          </Route>
-          <Route path="/">
-            <HomeView />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Navbar />
+        <div className="content-wrapper">
+          <Switch>
+            <Route path="/settings">
+              <SettingsView />
+            </Route>
+            <Route path="/login">
+              <LoginView />
+            </Route>
+            <Route path="/register">
+              <RegisterView />
+            </Route>
+            <Route path="/chat/:id">
+              <ChatView />
+            </Route>
+            <Route path="/">
+              <HomeView />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </Provider>
   );
 }
