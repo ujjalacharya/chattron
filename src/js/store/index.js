b1d0c16ee1bb6171a80494df84f7d622544a029e
Ujjal Acharya <ujjal.acharya@amniltech.com>
2020-10-30 14:46:46 +0545

    auth reducer

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,12 +1,14 @@
 import { createStore, applyMiddleware, combineReducers } from "redux";
 import thunkMiddleware from "redux-thunk";
 import chatReducer from "../reducers/chats";
+import authReducer from "../reducers/auth";
 
 export default function configureStore() {
   const middlewares = [thunkMiddleware];
   const store = createStore(
     combineReducers({
       chats: chatReducer,
+      auth: authReducer,
     }),
     applyMiddleware(...middlewares)
   );
