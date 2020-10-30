54b0fdb4e87c3b8ed1c8d5aca103150fdd53865b
Ujjal Acharya <ujjal.acharya@amniltech.com>
2020-10-30 14:37:09 +0545

    create user profile

--- src/js/api/auth.js
+++ src/js/api/auth.js
@@ -1,12 +1,22 @@
 import firebase from "firebase/app";
 import "firebase/auth";
+import db from "../db/firestore";
 
+const createUserProfile = (userProfile) =>
+  db.collection("profiles").doc(userProfile.uid).set(userProfile);
+  
 export async function register({ email, password }) {
   try {
     const { user } = await firebase
       .auth()
       .createUserWithEmailAndPassword(email, password);
-    return user;
+    await createUserProfile({
+      uid: user.uid,
+      username,
+      email,
+      avatar,
+      joinedChats: [],
+    });
   } catch (error) {
     return Promise.reject(error.message);
   }
