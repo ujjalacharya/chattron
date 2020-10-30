200fdbf96a2dea8493393119570f603f858763d9
Ujjal Acharya <ujjal.acharya@amniltech.com>
2020-10-30 16:57:22 +0545

    Login and register reducer creator

--- src/js/reducers/auth.js
+++ src/js/reducers/auth.js
@@ -1,62 +1,75 @@
-import { combineReducers } from "redux";
+import { combineReducers } from 'redux';
 
-function loginReducer(state = { error: null }, action) {
-  switch (action.type) {
-    case "AUTH_LOGIN_INIT":
-      return { error: null };
-    case "AUTH_LOGIN_ERROR":
-      return { error: action.error };
-    default:
-      return state;
+function createLoginReducer() {
+  const error = (state = null, action) => {
+    switch(action.type) {
+      case 'AUTH_LOGIN_INIT':
+        return null;
+      case 'AUTH_LOGIN_ERROR':
+        return action.error;
+      default:
+        return state;
+    }
   }
+
+  return combineReducers({
+    error
+  })
 }
 
-function registerReducer(state = { error: null }, action) {
-  switch (action.type) {
-    case "AUTH_REGISTER_INIT":
-      return { error: null };
-    case "AUTH_REGISTER_ERROR":
-      return { error: action.error };
-    default:
-      return state;
+function createRegisterReducer() {
+  const error = (state = null, action) => {
+    switch(action.type) {
+      case 'AUTH_REGISTER_INIT':
+        return null
+      case 'AUTH_REGISTER_ERROR':
+        return action.error;
+      default:
+        return state;
+    }
   }
+
+  return combineReducers({
+    error
+  })
 }
 
 function createAuthReducer() {
   const user = (state = null, action) => {
-    switch (action.type) {
-      case "AUTH_ON_ERROR":
-      case "AUTH_ON_INIT":
+    switch(action.type) {
+      case 'AUTH_ON_ERROR':
+      case 'AUTH_ON_INIT':
         return null;
-      case "AUTH_ON_SUCCESS":
+      case 'AUTH_ON_SUCCESS':
         return action.user;
       default:
         return state;
     }
-  };
+  }
 
   const isChecking = (state = false, action) => {
-    switch (action.type) {
-      case "AUTH_ON_INIT":
-      case "AUTH_REGISTER_INIT":
-      case "AUTH_LOGIN_INIT":
+    switch(action.type) {
+      case 'AUTH_ON_INIT':
+      case 'AUTH_REGISTER_INIT':
+      case 'AUTH_LOGIN_INIT':
         return true;
-      case "AUTH_ON_ERROR":
-      case "AUTH_ON_SUCCESS":
-      case "AUTH_LOGIN_ERROR":
-      case "AUTH_REGISTER_ERROR":
+      case 'AUTH_ON_ERROR':
+      case 'AUTH_ON_SUCCESS':
+      case 'AUTH_LOGIN_ERROR':
+      case 'AUTH_REGISTER_ERROR':
         return false;
       default:
         return state;
     }
-  };
+  }
 
   return combineReducers({
     user,
     isChecking,
-    login: loginReducer,
-    register: registerReducer,
-  });
+    login: createLoginReducer(),
+    register: createRegisterReducer()
+  })
 }
+
 
 export default createAuthReducer();
