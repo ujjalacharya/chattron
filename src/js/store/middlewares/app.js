6c20d829c0805b6318cac5021dba84af0488be45
Ujjal Acharya <ujjal.acharya@amniltech.com>
2020-10-31 16:00:54 +0545

    display connection notification

--- src/js/store/middlewares/app.js
+++ src/js/store/middlewares/app.js
@@ -1,8 +1,13 @@
+import Notification from "../../utils/notifications";
+
 export default (store) => (next) => (action) => {
   switch (action.type) {
     case "APP_IS_ONLINE":
     case "APP_IS_OFFLINE": {
-      alert("Displaying Notification!");
+      Notification.show({
+        title: "Connection status:",
+        body: action.isOnline ? "Online" : "Offline",
+      });
     }
   }
 
--- src/js/utils/notifications.js
+++ src/js/utils/notifications.js
@@ -2,6 +2,8 @@ export default {
   setup() {
     if (!("Notification" in window)) {
       console.error("This browser window doesn't support notification!");
+    } else if (Notification.permission === "granted") {
+      return;
     } else if (Notification.permission !== "denied") {
       Notification.requestPermission().then((permission) => {
         if (permission === "granted") {
@@ -9,5 +11,8 @@ export default {
         }
       });
     }
+  },
+  show({ title, body }) {
+    new Notification(title, { body });
   },
 };
