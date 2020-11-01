57ff5e6a7fd8fb4a9c0c6c5f4747250a8084d114
Ujjal Acharya <ujjal.acharya@amniltech.com>
2020-11-01 14:33:52 +0545

    chat create form

--- src/js/views/ChatCreate.js
+++ src/js/views/ChatCreate.js
@@ -1,8 +1,61 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import { withBaseLayout } from "../layouts/Base";
 
 function ChatCreate() {
-  return <h1>Chat Create</h1>;
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    alert(JSON.stringify(data));
+  };
+
+  return (
+    <div className="centered-view">
+      <div className="centered-container">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="centered-container-form"
+        >
+          <div className="header">Create chat now!</div>
+          <div className="subheader">Chat with people you know</div>
+          <div className="form-container">
+            <div className="form-group">
+              <label htmlFor="name">Name</label>
+              <input
+                ref={register}
+                type="text"
+                className="form-control"
+                id="name"
+                name="name"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="description">Description</label>
+              <textarea
+                ref={register}
+                name="description"
+                className="form-control"
+                id="description"
+              ></textarea>
+            </div>
+            <div className="form-group">
+              <label htmlFor="image">Image</label>
+              <input
+                ref={register}
+                type="text"
+                className="form-control"
+                id="image"
+                name="image"
+              />
+            </div>
+            <button type="submit" className="btn btn-outline-primary">
+              Create
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default withBaseLayout(ChatCreate, { canGoBack: true });
