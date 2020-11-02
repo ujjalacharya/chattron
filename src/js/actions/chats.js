905fec9b78784917549307cfada9eecb32f24ba6
Ujjal Acharya <ujjal.acharya@amniltech.com>
2020-11-02 14:38:01 +0545

    Create chat actions

--- src/js/actions/chats.js
+++ src/js/actions/chats.js
@@ -1,13 +1,26 @@
 import * as api from "../api/chats";
+import db from '../db/firestore';
 
 export function fetchChats() {
   return async function (dispatch) {
     const chats = await api.fetchChats();
     dispatch({
-        type: 'CHATS_FETCH_SUCCESS',
-        chats
-      })
-  
-      return chats;
+      type: "CHATS_FETCH_SUCCESS",
+      chats,
+    });
+
+    return chats;
   };
 }
+
+export const createChat = (formData, userId) => (dispatch) => {
+  debugger;
+  const newChat = { ...formData };
+  const useRef = db.doc(`profiles/${userId}`);
+  newChat.admin = useRef;
+  newChat.joinedUsers = [useRef];
+
+  return api
+    .createChat(newChat)
+    .then((_) => dispatch({ type: "CHATS_CREATE_SUCCESS" }));
+};
