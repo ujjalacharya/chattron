bdadc9cb7ff389d1fbeb07c59a321e268b3c9786
Ujjal Acharya <ujjal.acharya@amniltech.com>
2020-11-03 15:15:36 +0545

    Unsubscribe from user

--- src/js/views/Chat.js
+++ src/js/views/Chat.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import ChatUserList from "../components/ChatUsersList";
@@ -9,6 +9,7 @@ import { subscribeToChat, subscribeToProfile } from "../actions/chats";
 
 function Chat() {
   const { id } = useParams();
+  const peopleWatchers = useRef({});
   const dispatch = useDispatch();
   const activeChat = useSelector(({ chats }) => chats.activeChats[id]);
   const joinedUsers = activeChat?.joinedUsers;
@@ -17,6 +18,7 @@ function Chat() {
     const unsubFromChat = dispatch(subscribeToChat(id));
     return () => {
       unsubFromChat();
+      unsubFromJoinedUsers();
     };
   }, []);
 
@@ -26,8 +28,18 @@ function Chat() {
 
   const subscribeToJoinedUsers = (jUsers) => {
     jUsers.forEach((user) => {
-      dispatch(subscribeToProfile(user.uid));
+      if (!peopleWatchers.current[user.uid]) {
+        peopleWatchers.current[user.uid] = dispatch(
+          subscribeToProfile(user.uid)
+        );
+      }
     });
+  };
+
+  const unsubFromJoinedUsers = () => {
+    Object.keys(peopleWatchers.current).forEach((id) =>
+      peopleWatchers.current[id]()
+    );
   };
 
   return (
