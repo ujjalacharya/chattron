c7db865394916f0c955f598df6c179f33f1d7115
Ujjal Acharya <ujjal.acharya@amniltech.com>
2020-11-03 14:44:40 +0545

    display sorted chats

--- src/js/views/Home.js
+++ src/js/views/Home.js
@@ -11,7 +11,8 @@ import Notification from "../utils/notifications";
 
 function Home() {
   const dispatch = useDispatch();
-  const chats = useSelector(({ chats }) => chats.items);
+  const joinedChats = useSelector(({ chats }) => chats.joined);
+  const availableChats = useSelector(({ chats }) => chats.available);
 
   useEffect(() => {
     Notification.setup();
@@ -21,7 +22,7 @@ function Home() {
   return (
     <div className="row no-gutters fh">
       <div className="col-3 fh">
-        <JoinedChatsList chats={[]} />
+        <JoinedChatsList chats={joinedChats} />
       </div>
       <div className="col-9 fh">
         <ViewTitle text="Choose your channel">
@@ -29,7 +30,7 @@ function Home() {
             New
           </Link>
         </ViewTitle>
-        <AvailableChatsList chats={[]} />
+        <AvailableChatsList chats={availableChats} />
       </div>
     </div>
   );
