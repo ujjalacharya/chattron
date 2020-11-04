8b0a0378ca759d8698fe2072809d450f587eda29
Ujjal Acharya <ujjal.acharya@amniltech.com>
2020-11-04 21:02:08 +0545

    Clean state after logout

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -3,18 +3,27 @@ import thunkMiddleware from "redux-thunk";
 import chatReducer from "../reducers/chats";
 import authReducer from "../reducers/auth";
 import appReducer from "../reducers/app";
+
 import appMiddleware from "./middlewares/app";
 
 export default function configureStore() {
   const middlewares = [thunkMiddleware, appMiddleware];
-  const store = createStore(
-    combineReducers({
-      chats: chatReducer,
-      auth: authReducer,
-      app: appReducer,
-    }),
-    applyMiddleware(...middlewares)
-  );
+
+  const mainReducer = combineReducers({
+    chats: chatReducer,
+    auth: authReducer,
+    app: appReducer,
+  });
+
+  const rootReducer = (state, action) => {
+    if (action.type === "AUTH_LOGOUT_SUCCESS") {
+      state = undefined;
+    }
+
+    return mainReducer(state, action);
+  };
+
+  const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
   return store;
 }
