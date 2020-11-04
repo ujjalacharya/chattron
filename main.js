beb3857144380fbe69b67d26355d175000ce737d
Ujjal Acharya <ujjal.acharya@amniltech.com>
2020-11-04 21:38:24 +0545

    App menu

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Main Process
-const { app, BrowserWindow, ipcMain, Notification } = require('electron');
-const path = require('path');
+const { app, BrowserWindow, ipcMain, Notification, Menu } = require("electron");
+const path = require("path");
 const isDev = !app.isPackaged;
 
 function createWindow() {
@@ -8,7 +8,7 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
-    backgroundColor: "white",
+    backgroundColor: "#6e707e",
     webPreferences: {
       nodeIntegration: false,
       // will sanitize JS code
@@ -18,43 +18,46 @@ function createWindow() {
       // internal logic run in sparate context
       contextIsolation: true,
       worldSafeExecuteJavaScript: true,
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
 
-  win.loadFile('index.html')
+  win.loadFile("index.html");
   isDev && win.webContents.openDevTools();
 }
 
 if (isDev) {
-  require('electron-reload')(__dirname, {
-    electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
-  })
+  require("electron-reload")(__dirname, {
+    electron: path.join(__dirname, "node_modules", ".bin", "electron"),
+  });
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+  const template = require("./utils/Menu").createTemplate(app);
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+  createWindow();
+});
 
-ipcMain.on('notify', (_, message) => {
-  new Notification({title: 'Notification', body: message}).show();
-})
+ipcMain.on("notify", (_, message) => {
+  new Notification({ title: "Notification", body: message }).show();
+});
 
-ipcMain.on('app-quit', () => {
+ipcMain.on("app-quit", () => {
   app.quit();
-})
+});
 
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
-})
+});
 
-app.on('activate', () => {
+app.on("activate", () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
-})
-
+});
 
 // Chromium -> web eingine for rendering the UI, full Chrome-like web browser
 // V8 -> engine that provides capabilities to execute, run, JS code in the browser
